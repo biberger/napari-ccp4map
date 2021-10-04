b3979a0f990a9c6aa792bb82a805ad229ae03678
Simon Biberger <biberger@ebi.ac.uk>
2021-10-04 18:23:19 +0200

    removed the ccp4 file and changed the test accordingly

--- napari_ccp4map/_tests/test_reader.py
+++ napari_ccp4map/_tests/test_reader.py
@@ -1,19 +1,6 @@
 from napari_ccp4map import napari_get_reader
 from pathlib import Path
 
-def test_reader(tmp_path):
-    ccp4_file = Path(__file__).parent / "5wkd.ccp4"
-
-    # try to read it back in
-    reader = napari_get_reader(ccp4_file)
-    assert callable(reader)
-
-    # make sure we're delivering the right format
-    layer_data_list = reader(ccp4_file)
-    assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
-    layer_data_arr = layer_data_list[0]
-    assert isinstance(layer_data_arr, np.array) and len(layer_data_arr) > 0
-
 def test_get_reader_pass():
     reader = napari_get_reader("fake.file")
     assert reader is None
